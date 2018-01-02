c31a8a13a6ff949aed4b7408eb0ad2add200f9bc
Paul Brower <brower.paul@gmail.com>
2018-01-02 11:14:28 -0700

    copy over cpp files

---
+++ opencl/log.h
@@ -0,0 +1,16 @@
+//
+// Created by paul on 11/14/17.
+//
+
+#ifndef BROWER_WALK_LOG_H
+#define BROWER_WALK_LOG_H
+
+
+#include <stdio.h>
+
+int log(std::string str) {
+    std::cout << str << std::endl;
+    return 0;
+}
+
+#endif //BROWER_WALK_LOG_H
---
+++ opencl/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by paul on 11/15/17.
+//
+
+#include "brower_walk.h"
+
+int main() {
+
+    while (true) {
+        mine();
+    }
+
+    return 0;
+}
